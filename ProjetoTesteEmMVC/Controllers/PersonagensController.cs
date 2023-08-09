ba50d2692bd22fd1e4e9846c0ae464e0baf75223
ygor.lima <ygor.lima@IT2B001942>
2023-08-09 12:10:52 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoTesteEmMVC/Controllers/PersonagensController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoTesteEmMVC.Controllers
+{
+    public class PersonagensController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CriarPersonagem()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult Excluir()
+        {
+            return View();
+        }
+    }
+}
